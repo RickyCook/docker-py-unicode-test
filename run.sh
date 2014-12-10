5e31072bfbcb2cafa91ed800e53ba3bb81899363
Ricky Cook <rcook@odecee.com.au>
2014-12-10 19:04:18 +1100

    Update run to ignore local envs

--- run.sh
+++ run.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 echo "Trying python '$1'"
-case $1 in
-3*)
-	python_exec=python3
-	pip_exec=pip3
-	;;
-*)
-	python_exec=python
-	pip_exec=pip
-	;;
-esac
 
 python_location="$(which python$1)"
 echo "Using python at '$python_location'"
 
-virtualenv -p "$python_location" "py$1"
+venv_dir="py$1"
+virtualenv -p "$python_location" "$venv_dir"
 
-source "py$1/bin/activate"
+pip_exec="$venv_dir/bin/pip"
+python_exec="$venv_dir/bin/python"
+
 $pip_exec install docker-py
 $pip_exec freeze
 
 IMAGE_ID_RE='[a-z0-9]*'
-image_id=$($python_exec ./builder.py | tee /dev/stderr | grep -Eo "Successfully built $IMAGE_ID_RE" | grep -Eo "$IMAGE_ID_RE$")
+image_id=$($python_exec builder.py | tee /dev/stderr | grep -Eo "Successfully built $IMAGE_ID_RE" | grep -Eo "$IMAGE_ID_RE$")
 
 if [[ -z $image_id ]]; then
 	echo "No container found to destroy"
 else
 	docker rmi $image_id
 fi
-
-deactivate
